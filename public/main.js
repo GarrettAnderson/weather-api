14185468ee3e6bbaddf337ced99f0173171b94ba
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-12 13:30:29 -0500

    created a simple input and search button in the HTML. created the script to call the open weather api and return weather data for a zipcode

--- public/main.js
+++ public/main.js
@@ -1,7 +1,20 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let currentWeather = {}
+const appKey = 'aa1b094472a96c37a26f10903aedb34d'
+// user inputs a zip code or city name
+
+// click search button to query api
+const getWeather = () => {
+  let zipcode = document.querySelector('.search-weather').value
+  fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=' + appKey)
+    .then((resp) => {
+      return resp.json()
+    })
+    // open the package
+    .then((zipcode) => {
+      currentWeather = zipcode
+      console.log(currentWeather)
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.search-button').addEventListener('click', getWeather)
+// display the weather conditions via the DOM
