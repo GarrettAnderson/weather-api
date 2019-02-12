da0dfe405d92a336a12ab67188e6ade7bfa966d6
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-12 13:41:43 -0500

    added another input area and button to search by city name
    created another script to search by city name when search by city button is clicked

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ const appKey = 'aa1b094472a96c37a26f10903aedb34d'
 // user inputs a zip code or city name
 
 // click search button to query api
-const getWeather = () => {
+const getWeatherByZip = () => {
   let zipcode = document.querySelector('.search-weather').value
   fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=' + appKey)
     .then((resp) => {
@@ -16,5 +16,18 @@ const getWeather = () => {
     })
 }
 
-document.querySelector('.search-button').addEventListener('click', getWeather)
-// display the weather conditions via the DOM
+const getWeatherByCity = () => {
+  let city = document.querySelector('.search-weather-city').value
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',us&appid=' + appKey)
+    .then((resp) => {
+      return resp.json()
+    })
+    // open the package
+    .then((city) => {
+      currentWeather = city
+      console.log(currentWeather)
+    })
+}
+
+document.querySelector('.search-button').addEventListener('click', getWeatherByZip)
+document.querySelector('.search-button-city').addEventListener('click', getWeatherByCity)
