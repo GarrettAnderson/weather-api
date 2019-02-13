b4f24fb1ba65440d4416cee27f088287520bee5e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-12 22:08:38 -0500

    got the local storage object to store the zipcode entered in the input area

--- public/main.js
+++ public/main.js
@@ -1,18 +1,21 @@
 let currentWeather = {}
-const appKey = 'aa1b094472a96c37a26f10903aedb34d'
+const apiKey = 'aa1b094472a96c37a26f10903aedb34d'
 // user inputs a zip code or city name
 
 // click search button to query api
 const getWeatherByZip = () => {
   let zipcode = document.querySelector('.search-weather').value
-  fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&appid=' + appKey + '&units=imperial')
+  fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&appid=' + apiKey + '&units=imperial')
     .then((resp) => {
       return resp.json()
     })
     // open the package
-    .then((zipcode) => {
-      currentWeather = zipcode
-      console.log(currentWeather.weather[0])
+    .then((zip) => {
+      currentWeather = zip
+      // set local storage to save the zipcode
+      localStorage.setItem('zip', zipcode)
+      console.log(localStorage)
+      console.log(currentWeather)
       document.querySelector('.current-weather-display').textContent =
         currentWeather.weather[0].main + ' - ' + currentWeather.weather[0].description
     })
@@ -20,7 +23,7 @@ const getWeatherByZip = () => {
 
 const getWeatherByCity = () => {
   let city = document.querySelector('.search-weather-city').value
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=imperial')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial')
     .then((resp) => {
       return resp.json()
     })
@@ -34,5 +37,28 @@ const getWeatherByCity = () => {
   // .weather[0].main + ' - ' + currentWeather.weather[0].description
 }
 
+// on page load, automatically search user's current location using HTML 5 Geolocation API
+const main = () => {
+  const geo_success = (position) => {
+    console.log(position)
+  }
+  let watchId = navigator.geolocation.watchPosition(geo_success)
+  console.log(watchId)
+}
+
+// const determineZipOrCity = () => {
+// let zipcode = document.querySelector('.search-weather').value
+// let weatherCondition = currentWeather.weather[0].main
+// if the input value is not a number, assume it is a city and run getWeatherByCity function
+// if (zipcode === NaN) {
+//   console.log('searching by city')
+// }
+// let weatherCondition = currentWeather.weather[0].main
+// if (weatherCondition === 'Rain') {
+//   console.log('Cloudy')
+// }
+
 document.querySelector('.search-button').addEventListener('click', getWeatherByZip)
+// document.querySelector('.search-button').addEventListener('click', determineZipOrCity)
 document.querySelector('.search-button-city').addEventListener('click', getWeatherByCity)
+document.addEventListener('DOMContentLoaded', main)
